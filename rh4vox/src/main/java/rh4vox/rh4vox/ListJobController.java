1d032cf817c5c489875a8c9a672f58e8ee308ea9
João Vitor Marques <jvsilvam@outlook.com>
2022-09-30 11:34:38 -0300

    remove scrollbar on list jobs screen

--- rh4vox/src/main/java/rh4vox/rh4vox/ListJobController.java
+++ rh4vox/src/main/java/rh4vox/rh4vox/ListJobController.java
@@ -1,5 +1,22 @@
 package rh4vox.rh4vox;
 
-public class ListJobController {
-  
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+
+public class ListJobController implements Initializable {
+   
+    @FXML
+    private ScrollPane scrollContainer;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        scrollContainer.setHbarPolicy(ScrollBarPolicy.NEVER);
+        scrollContainer.setVbarPolicy(ScrollBarPolicy.NEVER);
+      
+    }
 }

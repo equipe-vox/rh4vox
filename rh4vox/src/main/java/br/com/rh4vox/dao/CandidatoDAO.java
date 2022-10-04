9fbf7760f87c8f1e4bae55cd3f5b906ee0e0ae15
João Vitor Marques <jvsilvam@outlook.com>
2022-10-04 10:38:14 -0300

    create Candidato DAO

---
+++ rh4vox/src/main/java/br/com/rh4vox/dao/CandidatoDAO.java
@@ -0,0 +1,27 @@
+package br.com.rh4vox.dao;
+
+import java.util.List;
+
+import br.com.rh4vox.model.Candidato; 
+
+public class CandidatoDAO {
+  public Candidato getCandidato() {
+    return null;
+  }
+
+  public void insertCandidato(Candidato candidato) {
+
+  }
+
+  public void updateCandidato(Candidato candidato) {
+
+  }
+
+  public void removeCandidato(int id) {
+
+  }
+
+  public List<Candidato> listCandidatos() {
+    return null;
+  }
+}

172fb8bd65f8f75dc7d4f2a077b5a4622a1cdc18
Hamilton Zanini <hrszanini@gmail.com>
2022-10-08 13:29:33 -0300

    ALteração de banco de dados e correção menor de SQL

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,19 +1,30 @@
+CREATE TABLE vaga (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR NOT NULL,
+	descricao TEXT NOT NULL,
+	salario NUMERIC NOT NULL,
+	regime VARCHAR NOT NULL,
+	aberto BOOLEAN NOT NULL,
+	negociavel BOOLEAN NOT NULL,
+	cargo VARCHAR NOT NULL
+);
+
 CREATE TABLE usuario (
 	id SERIAL PRIMARY KEY,
 	email TEXT NOT NULL UNIQUE,
 	senha VARCHAR NOT NULL,
 	tipo VARCHAR NOT NULL
-)
+);
 
 CREATE TABLE candidato (
 	id SERIAL PRIMARY KEY,
 	nome TEXT NOT NULL,
 	data_nasc DATE NOT NULL,
 	cpf VARCHAR NOT NULL,
-		id_usuario INTEGER NOT NULL,
+	id_usuario INTEGER NOT NULL,
 	FOREIGN KEY (id_usuario) 
           REFERENCES usuario (id)	
-)
+);
 
 CREATE TABLE curriculo (
 	id SERIAL PRIMARY KEY,
@@ -24,19 +35,8 @@ CREATE TABLE curriculo (
 	experiencia VARCHAR NOT NULL,
 	id_candidato INTEGER NOT NULL,
 	FOREIGN KEY (id_candidato) 
-          REFERENCES candidato (id),
-)
-
-CREATE TABLE vaga (
-	id SERIAL PRIMARY KEY,
-	nome VARCHAR NOT NULL,
-	descricao TEXT NOT NULL,
-	salario NUMERIC NOT NULL,
-	regime VARCHAR NOT NULL,
-	aberto BOOLEAN NOT NULL,
-	negociavel BOOLEAN NOT NULL,
-	cargo VARCHAR NOT NULL,
-)
+          REFERENCES candidato (id)
+);
 
 CREATE TABLE candidato_vaga (
 	id SERIAL PRIMARY KEY,
@@ -47,7 +47,7 @@ CREATE TABLE candidato_vaga (
           REFERENCES candidato (id),
 	FOREIGN KEY (id_vaga) 
           REFERENCES vaga (id)
-)
+);
 
 CREATE TABLE usuario_vaga (
 	id SERIAL PRIMARY KEY,
@@ -57,4 +57,4 @@ CREATE TABLE usuario_vaga (
           REFERENCES usuario (id),
 	FOREIGN KEY (id_vaga) 
           REFERENCES vaga (id)
-)
+);

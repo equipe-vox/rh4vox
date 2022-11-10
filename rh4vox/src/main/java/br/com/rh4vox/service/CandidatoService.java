5cc10919235b76b660fbd7e9504baf2843fa3c6e
Hamilton Zanini <hrszanini@gmail.com>
2022-11-09 21:36:46 -0300

    Adição de classe de teste

--- rh4vox/src/main/java/br/com/rh4vox/service/CandidatoService.java
+++ rh4vox/src/main/java/br/com/rh4vox/service/CandidatoService.java
@@ -46,4 +46,35 @@ public class CandidatoService {
   public void updateCandidato(String nome, String telefone, Integer id) throws SQLException {
     dao.updateCandidato(nome, telefone, id);
   }
+  
+  public boolean checkCpf(String cpf){
+    String cpfNumbers = cpf.replace(".", "").replace("-", "");
+    int primeiroDigito = Character.getNumericValue(cpfNumbers.charAt(9));
+    int segundoDigito = Character.getNumericValue(cpfNumbers.charAt(10));
+    int somaPrimeiroDigito = 0, somaSegundoDigito = 0, sobra;
+    
+    for(int i=0; i<9; i++){
+      int numberAtPosition = Character.getNumericValue(cpfNumbers.charAt(i));
+      somaPrimeiroDigito += numberAtPosition * (10-i);
+    }
+
+    sobra = 11 - (somaPrimeiroDigito % 11);
+
+    if(sobra > 10 && primeiroDigito != 0 || sobra < 10 && sobra != primeiroDigito){
+      return false;
+    }
+
+    for(int i=0; i<10; i++){
+      int numberAtPosition = Character.getNumericValue(cpfNumbers.charAt(i));
+      somaSegundoDigito += numberAtPosition * (11-i);
+    }
+
+    sobra = 11 - (somaSegundoDigito % 11);
+
+    if(sobra > 10 && segundoDigito != 0 || sobra < 10 && sobra != segundoDigito){
+      return false;
+    }
+
+    return true;
+  }
 }
